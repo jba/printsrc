85e3421d52d232c0f86b2a8978f5bc7f007d7319
Jonathan Amsterdam <jba@google.com>
2021-12-11 12:10:33 -0500

    finished all kinds

--- print.go
+++ print.go
@@ -1,5 +1,12 @@
 // Print a Go value as Go source.
 
+// TODO:
+// - elision
+// - imports
+// - anonymous types
+// - ptr cycle detection
+// - omit zero-valued struct fields
+
 package main
 
 import (
@@ -17,19 +24,24 @@ type optFunc func(*state) error
 
 type PrintFunc func(interface{}) (string, error)
 
-type state struct {
-	w              io.Writer
-	homePackage    string
-	err            error
-	customPrinters map[reflect.Type]PrintFunc
-	importsNeeded  map[string]bool
+type Import struct {
+	Identifier string
+	Path       string
 }
 
-func (s *state) addImport(pkg string) {
+type state struct {
+	w              io.Writer
+	pkg            string
+	err            error
+	customPrinters map[reflect.Type]PrintFunc
+	importsNeeded  map[string]Import
+}
+
+func (s *state) addImport(importPath string) {
 	if s.importsNeeded == nil {
-		s.importsNeeded = map[string]bool{}
+		s.importsNeeded = map[string]Import{}
 	}
-	s.importsNeeded[pkg] = true
+	// s.importsNeeded[importPath] = Import{id, pkg}b
 }
 
 func Use(tv interface{}, f PrintFunc) Option {
@@ -55,7 +67,10 @@ func Print(value interface{}, opts ...Option) error {
 }
 
 func Fprint(w io.Writer, value interface{}, opts ...Option) error {
-	s := &state{w: w}
+	s := &state{
+		w:   w,
+		pkg: "printing", // TODO: generalize
+	}
 	for _, opt := range opts {
 		if err := opt(s); err != nil {
 			return err
@@ -69,117 +84,221 @@ func (s *state) print(value interface{}) {
 	s.rprint(reflect.ValueOf(value))
 }
 
-func (s *state) rprint(rv reflect.Value) error {
+func (s *state) rprint(rv reflect.Value) {
 	if !rv.IsValid() {
-		return s.printString("nil") // TODO: may need a cast
+		s.printString("nil") // TODO: may need a cast
+		return
 	}
 	if cp := s.customPrinters[rv.Type()]; cp != nil {
 		out, err := cp(rv.Interface())
 		if err != nil {
-			return err
+			s.err = err
+			return
 		}
-		return s.printString(out)
+		s.printString(out)
+		return
 	}
 
 	switch rv.Kind() {
 	case reflect.Bool, reflect.String, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return s.printf("%#v", rv.Interface())
+		s.printf("%#v", rv.Interface())
 	case reflect.Float32, reflect.Float64:
-		return s.printFloat(rv)
+		s.printFloat(rv)
 	case reflect.Complex64, reflect.Complex128:
-		//return s.printComplex(rv.Complex())
+		s.printComplex(rv)
 	case reflect.Ptr:
-		// TODO: cycle detection
-		return s.printPtr(rv.Elem())
-	case reflect.Slice:
-		//return s.printSlice(rv)
-	case reflect.Array:
+		s.printPtr(rv.Elem())
 	case reflect.Interface:
+		s.rprint(rv.Elem())
+	case reflect.Slice, reflect.Array:
+		s.printSliceOrArray(rv)
 	case reflect.Map:
+		s.printMap(rv)
 	case reflect.Struct:
+		s.printStruct(rv)
 	case reflect.Chan, reflect.Func, reflect.UnsafePointer:
-		return fmt.Errorf("cannot print %#v as source", rv.Interface())
+		s.err = fmt.Errorf("cannot print %#v as source", rv.Interface())
 	default:
 		panic("bad kind")
 	}
-	return nil
 }
 
-func (s *state) rsprint(rv reflect.Value) (string, error) {
+func (s *state) rsprint(rv reflect.Value) string {
 	s2 := *s
 	var buf bytes.Buffer
 	s2.w = &buf
-	if err := s.rprint(rv); err != nil {
-		return "", err
-	}
-	return buf.String(), nil
+	s2.rprint(rv)
+	return buf.String()
 }
 
-func (s *state) printFloat(rf reflect.Value) error {
+func (s *state) printFloat(rf reflect.Value) {
 	f := rf.Float()
+	tFloat64 := reflect.TypeOf(float64(0))
+	var str string
 	switch {
 	case math.IsNaN(f):
-		return s.printConversion(rf.Type(), reflect.TypeOf(float64(0)), "math.NaN()")
+		str = "math.NaN()"
 	case math.IsInf(f, 1):
-		return s.printConversion(rf.Type(), reflect.TypeOf(float64(0)), "math.Inf(1)")
+		str = "math.Inf(1)"
 	case math.IsInf(f, -1):
-		return s.printConversion(rf.Type(), reflect.TypeOf(float64(0)), "math.Inf(-1)")
+		str = "math.Inf(-1)"
 	default:
 		// No conversion needed for constant.
-		return s.printf("%#v", f)
+		s.printf("%#v", f)
+		return
+	}
+	s.printConversion(rf.Type(), tFloat64, str)
+}
+
+func (s *state) printComplex(rc reflect.Value) {
+	c := rc.Complex()
+	s.printf("complex(%s, %s)", s.rsprint(reflect.ValueOf(real(c))), s.rsprint(reflect.ValueOf(imag(c))))
+}
+
+func (s *state) printPtr(elem reflect.Value) {
+	if isPrimitive(elem.Kind()) {
+		s.printf("func() *%s { var x %[1]s = %s; return &x }()", s.sprintType(elem.Type()), s.rsprint(elem))
+	} else {
+		s.printString("&")
+		s.rprint(elem)
 	}
 }
 
-func (s *state) printPtr(elem reflect.Value) error {
-	switch elem.Kind() {
-	case reflect.Bool, reflect.String, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
-		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
-
-		selem, err := s.rsprint(elem)
-		if err != nil {
-			return err
+func (s *state) printSliceOrArray(rv reflect.Value) {
+	k := rv.Type().Elem().Kind()
+	s.printf("%s{", s.sprintType(rv.Type()))
+	if k != reflect.String && isPrimitive(k) && rv.Len() <= 10 {
+		// Write on one line.
+		for i := 0; i < rv.Len(); i++ {
+			if i > 0 {
+				s.printString(", ")
+			}
+			s.rprint(rv.Index(i))
 		}
-		return s.printf("func() *%s { x := %[1]s(%s); return &x }()", elem.Type(), selem)
+		s.printString("}")
+	} else {
+		s.printString("\n")
+		for i := 0; i < rv.Len(); i++ {
+			s.printString("\t")
+			s.rprint(rv.Index(i))
+			s.printString(",\n")
+		}
+		s.printString("}\n")
+	}
+}
+
+func (s *state) printMap(rv reflect.Value) {
+	switch rv.Len() {
+	case 0:
+		s.printf("%s{}", s.sprintType(rv.Type()))
+	case 1:
+		s.printf("%s{", s.sprintType(rv.Type()))
+		k := rv.MapKeys()[0]
+		s.rprint(k)
+		s.printString(": ")
+		s.rprint(rv.MapIndex(k))
+		s.printString("}")
 	default:
-		if err := s.printString("&"); err != nil {
-			return err
+		s.printf("%s{\n", s.sprintType(rv.Type()))
+		iter := rv.MapRange()
+		for iter.Next() {
+			// BUG: multiple NaNs will result in duplicate key, I think.
+			// TODO: random key order makes tests flaky.
+			s.printString("\t")
+			s.rprint(iter.Key())
+			s.printString(": ")
+			s.rprint(iter.Value())
+			s.printString(",\n")
 		}
-		return s.rprint(elem)
+		s.printString("}\n")
 	}
 }
 
-func (s *state) printString(str string) error {
-	_, err := io.WriteString(s.w, str)
-	return err
-}
-
-func (s *state) printf(format string, args ...interface{}) error {
-	_, err := fmt.Fprintf(s.w, format, args...)
-	return err
-}
-
-func err2(err1, err2 error) error {
-	if err1 != nil {
-		return err1
+func (s *state) printStruct(rv reflect.Value) {
+	t := rv.Type()
+	numExported := 0
+	for i := 0; i < t.NumField(); i++ {
+		if t.Field(i).IsExported() {
+			numExported++
+		}
 	}
-	return err2
-}
-
-func (s *state) printConversion(t, b reflect.Type, expr string) error {
-	if t == b || s.canElide(t) {
-		return s.printString(expr)
+	if numExported < 2 {
+		s.printf("%s{", s.sprintType(t))
+		for i := 0; i < t.NumField(); i++ {
+			f := t.Field(i)
+			if f.IsExported() {
+				s.printf("%s: ", f.Name)
+				s.rprint(rv.Field(i))
+			}
+		}
+		s.printString("}")
+	} else {
+		s.printf("%s{\n", s.sprintType(t))
+		for i := 0; i < t.NumField(); i++ {
+			f := t.Field(i)
+			if f.IsExported() {
+				s.printf("\t%s: ", f.Name)
+				s.rprint(rv.Field(i))
+				s.printf(",\n")
+			}
+		}
+		s.printf("}\n")
 	}
-	return err2(s.printType(t), s.printf("(%s)", expr))
-}
-
-func (s *state) printType(t reflect.Type) error {
-	if t.PkgPath() == s.homePackage {
-		return s.printString(t.Name())
-	}
-	return s.printString(t.String())
 }
 
 func (s *state) canElide(t reflect.Type) bool {
 	return false
+}
+
+func (s *state) sprintType(t reflect.Type) string {
+	switch t.Kind() {
+	case reflect.Ptr:
+		return "*" + s.sprintType(t.Elem())
+	case reflect.Slice:
+		return "[]" + s.sprintType(t.Elem())
+	case reflect.Array:
+		return fmt.Sprintf("[%d]", t.Len()) + s.sprintType(t.Elem())
+	case reflect.Map:
+		return "map[" + s.sprintType(t.Key()) + "]" + s.sprintType(t.Elem())
+	default:
+		if t.Name() == "" {
+			s.err = fmt.Errorf("can't handle unnamed type %s", t)
+			return "???"
+		}
+		if t.PkgPath() == s.pkg {
+			return t.Name()
+		}
+		return t.String()
+	}
+}
+
+func (s *state) printConversion(t, b reflect.Type, expr string) {
+	if t == b || s.canElide(t) {
+		s.printString(expr)
+	} else {
+		s.printf("%s(%s)", s.sprintType(t), expr)
+	}
+}
+
+func (s *state) printString(str string) {
+	if s.err == nil {
+		_, s.err = io.WriteString(s.w, str)
+	}
+}
+
+func (s *state) printf(format string, args ...interface{}) {
+	if s.err == nil {
+		_, s.err = fmt.Fprintf(s.w, format, args...)
+	}
+}
+
+func isPrimitive(k reflect.Kind) bool {
+	switch k {
+	case reflect.Bool, reflect.String, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
+		reflect.Float32, reflect.Float64, reflect.Complex64, reflect.Complex128:
+		return true
+	default:
+		return false
+	}
 }
--- print_test.go
+++ print_test.go
@@ -5,6 +5,21 @@ import (
 	"testing"
 )
 
+type T struct {
+	Boo bool
+	Map map[string]Float
+	u   int
+}
+
+type nesting struct {
+	A int
+	Nested
+}
+
+type Nested struct {
+	B int
+}
+
 func TestPrint(t *testing.T) {
 	i := int8(7)
 	f := Float(math.NaN())
@@ -24,16 +39,31 @@ func TestPrint(t *testing.T) {
 		{math.NaN(), "math.NaN()"},
 		{math.Inf(3), "math.Inf(1)"},
 		{math.Inf(-3), "math.Inf(-1)"},
-		{&i, "func() *int8 { x := int8(7); return &x }()"},
+		{complex(1, -1), "complex(1, -1)"},
+		{&i, "func() *int8 { var x int8 = 7; return &x }()"},
 		{f, "Float(math.NaN())"},
-		{&f, "func() *Float { x := Float(math.NaN()); return &x }()"},
+		{&f, "func() *Float { var x Float = Float(math.NaN()); return &x }()"},
+		{[]int{1, 2}, "[]int{1, 2}"},
+		{[]Float{2.3}, "[]Float{2.3}"},
+		{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, "[]int{\n\t1,\n\t2,\n\t3,\n\t4,\n\t5,\n\t6,\n\t7,\n\t8,\n\t9,\n\t10,\n\t11,\n}\n"},
+		{[1]bool{true}, "[1]bool{true}"},
+		{[]string{"a", "b"}, "[]string{\n\t\"a\",\n\t\"b\",\n}\n"},
+		{map[string]int{}, "map[string]int{}"},
+		{map[string]int{"a": 1}, `map[string]int{"a": 1}`},
+		//{map[string]int{"a": 1, "b": 2}, "map[string]int{\n\t\"a\": 1,\n\t\"b\": 2,\n}\n"},
+		{
+			T{false, map[string]Float{"x": 0.5}, 1},
+			"T{\n\tBoo: false,\n\tMap: map[string]Float{\"x\": 0.5},\n}\n",
+		},
+		{&Nested{B: 3}, "&Nested{B: 3}"},
+		{nesting{A: 1, Nested: Nested{B: 2}}, "nesting{\n\tA: 1,\n\tNested: Nested{B: 2},\n}\n"},
 	} {
 		got, err := Sprint(test.in)
 		if err != nil {
 			t.Fatal(err)
 		}
 		if got != test.want {
-			t.Errorf("%#v: got %s, want %s", test.in, got, test.want)
+			t.Errorf("%#v: got\n%s\nwant\n%s", test.in, got, test.want)
 		}
 	}
 }
